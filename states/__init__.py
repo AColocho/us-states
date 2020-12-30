d8598a2d2b1e34af16fef41c3759ee396bef90e4
Alejandro <a.colocho@outlook.com>
2020-12-30 13:44:40 -0500

    addid territories and docs

---
+++ states/__init__.py
@@ -0,0 +1,6 @@
+from .states import States_Abbreviated
+from .states import States_Full_Name
+from .territories import Territories_Abbreviated
+from .territories import Territories_Full_Name
+from .territories import Associated_States
+from .territories import Uninhabited_Territories
